588ac7f969d890f95ca019e990b6653df914f4b4
ppacris <arnnielkhenethpacris.cgc@gmail.com>
2023-05-31 01:42:16 +0800

    first commit

---
+++ script4-mobile.js
@@ -0,0 +1,39 @@
+var navDrawer = $('.nav__drawer__wrapper');
+var navActive = 'nav__drawer__active';
+var navOverlay = $('.nav__btn--overlay');
+var navBtn = $('.nav__btn');
+var navContent = $('.nav__drawer__content ul');
+
+$('.nav__btn').click(function () {
+	navDrawer.addClass(navActive);
+	navOverlay.fadeIn(1);
+	navBtn.fadeOut(1);
+	navContent.fadeIn(1);
+});
+
+$('.nav__close__btn').click(function () {
+	navDrawer.removeClass(navActive);
+	navContent.fadeOut(1);
+	navOverlay.fadeOut(1);
+	navBtn.fadeIn(400);
+});
+
+$('.nav__drawer__content ul li a').click(function () {
+	navDrawer.removeClass(navActive);
+	navContent.fadeOut(1);
+	navOverlay.fadeOut(1);
+	navBtn.fadeIn(400);
+});
+
+
+$(document).mousedown(function (e) {
+	var container = navDrawer;
+
+	if (!container.is(e.target) &&
+		container.has(e.target).length == 0) {
+
+		container.removeClass(navActive);
+		navOverlay.fadeOut(1);
+		navBtn.fadeIn(1);
+	}
+});
